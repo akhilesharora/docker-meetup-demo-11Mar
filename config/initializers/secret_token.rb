30a90cc3697a1cf86f5508cfdd6bd8400630e4b7
Akhilesh Arora <akhildawra@gmail.com>
2017-03-11 11:46:34 +0530

    docker-meetup-demo-app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = 'b4649ba8e0fa00d8de6b9ee4b0406616c94224fba726a0ee67cd352dc9c9634cd28503747fbed327b71744b9090ff70dc248bd614ca05635d66a3ee4e5a0b0b7'
